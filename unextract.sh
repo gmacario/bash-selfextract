3850ce835ad6fbf48736d601b5b2bee530f4b17f
Gianpaolo Macario <gmacario@gmail.com>
2018-05-27 19:41:32 +0200

    Fetch INFILE if it does not exist
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- unextract.sh
+++ unextract.sh
@@ -7,6 +7,8 @@ INFILE="iottlyagent_1.6.4_linux_raspberry-pi.bsx"
 OUTFILE="iottlyagent_1.6.4_linux_raspberry-pi.tar.xz"
 TMPDIR="./tmp-archive"
 
+[ ! -e $INFILE ] && wget https://cloud.iottly.com/getagent/$INFILE
+
 ARCHIVE=$(awk '/^__ARCHIVE_BELOW__/ {print NR + 1; exit 0; }' $INFILE)
 tail -n+$ARCHIVE $INFILE >$OUTFILE
 mkdir -p $TMPDIR
